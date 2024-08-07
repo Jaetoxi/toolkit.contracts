8d01061cb710d38b03e2aeffd9819898d0c7ba32
bull <bullbull@gmail.com>
2024-08-07 15:46:28 +0800

    update

--- scripts/create_bbp_account.sh
+++ scripts/create_bbp_account.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cat voter.txt | while IFS= read -r line; do
   # 在这里对每一行的内容做操作
+    echo "-----------create account: $line ------------"
     acct=$line
     creator=bbp
     owner=amax.dao@active
