770479b850855867fe07d85cc3eeab4ce13e9c61
bull <bullbull@gmail.com>
2024-08-07 15:43:59 +0800

    update

---
+++ scripts/create_bbp_account.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat voter.txt | while IFS= read -r line; do
+  # 在这里对每一行的内容做操作
+    acct=$line
+    creator=bbp
+    owner=amax.dao@active
+    activer=bbpadmin@active
+    amcli -u http://sh-amnod.vmi.amax.dev:18188 system newaccount --stake-net "0.005000 AMAX" --stake-cpu "0.005000 AMAX" --buy-ram-kbytes 4 $creator $acct $owner $activer -p bbp
+done
